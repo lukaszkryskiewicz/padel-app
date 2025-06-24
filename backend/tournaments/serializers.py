61c16d2c661a6e2fb7705196d8bcb943d6012254
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2025-06-24 13:56:26 +0200

    Add Torunament and Player serializers

---
+++ backend/tournaments/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+
+from backend.tournaments.models import Player, Tournament
+from django.db import transaction, IntegrityError
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['id','name', 'tournament']
+
+class PlayerCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['name']
+
+
+class TournamentSerializer(serializers.ModelSerializer):
+    players = PlayerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Tournament
+        fields = ['id', 'title', 'format', 'number_of_courts', 'points_per_match', 'created_at', 'players']
+
+
+class TournamentCreateSerializer(serializers.ModelSerializer):
+    players = PlayerCreateSerializer(many=True)
+
+    class Meta:
+        model = Tournament
+        fields = ['title', 'format', 'number_of_courts', 'points_per_match', 'players']
+
+    def validate(self, data):
+        names = [player['name'] for player in data['players']]
+        if len(names) != len(set(names)):
+            raise serializers.ValidationError({
+                "players": "Player names must be unique within a tournament."
+            })
+        return data
+
+    def create(self, validated_data):
+        players_data = validated_data.pop('players')
+
+        # double check for duplicates - editing players will be possible
+        try:
+            with transaction.atomic():
+                tournament = Tournament.objects.create(**validated_data)
+
+                for player_data in players_data:
+                    Player.objects.create(tournament=tournament, **player_data)
+
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {"error": "Duplicate player names in tournament"}
+            )
+
+        return tournament
