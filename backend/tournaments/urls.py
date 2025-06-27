852a33cc59a20f3479554a471749bd0b165670ea
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2025-06-27 23:23:56 +0200

    Add tournament app urls

---
+++ backend/tournaments/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from backend.tournaments.views import TournamentListCreateView, TournamentRetriveView, MatchListView, \
+    CurrentRoundMatchesView, MatchUpdateView, RoundResultsUpdateView
+
+urlpatterns = [
+    path('tournaments/', TournamentListCreateView.as_view(), name="tournament-list-create"),
+    path('tournament/<int:pk>/', TournamentRetriveView.as_view(), name="tournament-retrieve"),
+    path('tournament/<int:pk>/matches/', MatchListView.as_view(), name="match-list"),
+    path('tournament/<int:tournament_id>/current-round/', CurrentRoundMatchesView.as_view(), name="current-round-matches"),
+    path('tournament/<int:tournament_id>/update-single/<int:pk>/', MatchUpdateView.as_view(), name='single-match-update'),
+    path('tournament/<int:tournament_id>/update-round/', RoundResultsUpdateView.as_view(), name='round-results-update')
+]
